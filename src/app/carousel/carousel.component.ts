7acbaba7be8c8a207f80b37dbc37086ec2313369
sbuddha <sbuddha@ziperase.com>
2020-04-19 18:42:01 +0530

    center align nav, bg color, nav position

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -9,7 +9,7 @@ import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/n
 })
 export class CarouselComponent implements OnInit {
 
-  images = [62, 83, 466, 965, 982, 1043].map((n) => `https://picsum.photos/id/${n}/2000/500`);
+  images = [62, 83, 466, 965, 982, 1043].map((n) => `https://picsum.photos/id/${n}/2000/350`);
   k = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec vehicula nulla.",
   "Suspendisse pellentesque ex a enim ornare pharetra. ",
   "Fusce ac posuere nulla. Aliquam non neque ut diam blandit rutrum sed at lectus.",
